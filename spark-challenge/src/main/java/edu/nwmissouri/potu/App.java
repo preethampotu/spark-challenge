579eaeffb81f31337db443a996b37eef0d84a806
preethampotu <preetham.reddy16@gmail.com>
2019-11-20 09:30:13 -0600

    Add results file to the folder

--- spark-challenge/src/main/java/edu/nwmissouri/potu/App.java
+++ spark-challenge/src/main/java/edu/nwmissouri/potu/App.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Comparator;
+import org.apache.commons.io.FileUtils;
 
 /**
  * Hello world!
@@ -34,7 +35,15 @@ public final class App {
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("Challenge");
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
         JavaRDD<String> inputFile = sparkContext.textFile(fname);
-        
-
+        JavaRDD<String> wordsFromFile = inputFile.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
+        JavaPairRDD<String, Integer> countData = wordsFromFile.mapToPair(t -> new Tuple2(t, 1))
+        .reduceByKey((x, y) -> (int) x + (int) y);
+        JavaPairRDD<Integer, String> output = countData.mapToPair(p -> new Tuple2(p._2, p._1))
+        .sortByKey(Comparator.reverseOrder());
+        String outputFolder = "results";
+        Path path = FileSystems.getDefault().getPath(outputFolder);
+        FileUtils.deleteQuietly(path.toFile());
+        output.saveAsTextFile(outputFolder);
+        sparkContext.close();
     }
 }
